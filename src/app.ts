9b06f98142da29c98c00488aec384bd0deb1a310
freemjstudio <mjwoo0@naver.com>
2024-11-05 10:55:02 +0900

    change into lambda function

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
-import express, { Request, Response } from 'express';
-
-const app = express();
-const port = 3000;
-
-// GET 요청 핸들러 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello World");
-});
-
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+export const handler = async () => {
+    console.log('Hello from Lambda')
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'hello' })
+    };
+};
